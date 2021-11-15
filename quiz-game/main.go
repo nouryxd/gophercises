8d6c9856b2d751fdeaebc4becd2db30f3e558e49
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-15 16:54:19 +0100

    part 1

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,24 +14,28 @@ func main() {
 
 	file, err := os.Open(*csvFilename)
 	if err != nil {
-
-		fmt.Printf("Failed to open the csv file: %s", *csvFilename)
-		os.Exit(1)
+		exit(fmt.Sprintf("Failed to open the csv file: %s\n", *csvFilename))
 	}
 
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
 	if err != nil {
-		exit("Error while reading the csv file")
+		exit("Error reading the provided csv file")
 	}
 
 	problems := parseLines(lines)
 
+	correct := 0
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s\n", i+1, p.Question)
-
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
 	}
 
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
 }
 
 func parseLines(lines [][]string) []problem {
@@ -38,8 +43,8 @@ func parseLines(lines [][]string) []problem {
 
 	for i, line := range lines {
 		ret[i] = problem{
-			Question: line[0],
-			Answer:   line[1],
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
 		}
 	}
 
@@ -47,8 +52,8 @@ func parseLines(lines [][]string) []problem {
 }
 
 type problem struct {
-	Question string
-	Answer   string
+	q string
+	a string
 }
 
 func exit(msg string) {
