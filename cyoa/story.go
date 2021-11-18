bdeeaa311f6a30a44eb8fe4477e9ca9a4b5e3013
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-18 23:38:46 +0100

    bit of styling

--- cyoa/story.go
+++ cyoa/story.go
@@ -21,28 +21,65 @@ var defaultHandlerTmpl = `
   <head>
     <meta charset="utf-8">
     <title>Choose Your Own Adventure</title>
-    <link rel="stylesheet" href="css/style.css">
   </head>
-
   <body>
-    <h1>{{.Title}}</h1>
-
+    <section class="page">
+      <h1>{{.Title}}</h1>
       {{range .Paragraphs}}
         <p>{{.}}</p>
       {{end}}
-        
-        {{if .Options}}
-          <ul>
-            {{range .Options}}
-              <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-            {{end}}
-          </ul>
-        {{else}}
-          <h3>The End</h3>
+      {{if .Options}}
+        <ul>
+        {{range .Options}}
+          <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
         {{end}}
+        </ul>
+      {{else}}
+        <h3>The End</h3>
+      {{end}}
+    </section>
+    <style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FFFCF6;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #777;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: none;
+        color: #6295b5;
+      }
+      a:active,
+      a:hover {
+        color: #7792a2;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
   </body>
-</html>
-`
+</html>`
 
 func NewHandler(s Story) http.Handler {
 	return handler{s}
@@ -86,7 +123,7 @@ type Story map[string]Chapter
 
 type Chapter struct {
 	Title      string   `json:"title"`
-	Paragraphs []string `json:"paragraphs"`
+	Paragraphs []string `json:"story"`
 	Options    []Option `json:"options"`
 }
 
