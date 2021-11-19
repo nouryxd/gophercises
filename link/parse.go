d91b5c9f6e23fa8dce5ffd329ae218a8128285ae
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-19 16:55:30 +0100

    build link structs

--- link/parse.go
+++ link/parse.go
@@ -23,10 +23,24 @@ func Parse(r io.Reader) ([]Link, error) {
 	}
 
 	nodes := linkNodes(doc)
+	var links []Link
 	for _, node := range nodes {
+		links = append(links, buildLink(node))
 		fmt.Println(node)
 	}
-	return nil, nil
+	return links, nil
+}
+
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
+	}
+	ret.Text = "TODO: Parse the text..."
+	return ret
 }
 
 func linkNodes(n *html.Node) []*html.Node {
