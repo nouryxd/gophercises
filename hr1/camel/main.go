0ec9f97ba02f6fd5ec50d5e1bf265f2ed1c96418
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-22 15:17:25 +0100

    add camcelcase solution

--- hr1/camel/main.go
+++ hr1/camel/main.go
@@ -1,19 +1,36 @@
 package main
 
-import "fmt"
-
-var input = "oneTwoThreeFourFive"
+import (
+	"fmt"
+)
 
 func main() {
-	foundWords := 0
+	var input string
+	fmt.Scanf("%s\n", &input)
+	fmt.Println("Input is:", input)
 
+	// Solution 1
+	// answer := 1
+	//for _, ch := range input {
+	//	min, max := 'A', 'Z'
+	//	if ch >= min && ch <= max {
+	//		answer++
+	//	}
+
+	// Solution 2
+	// str := string(ch)
+	// if strings.ToUpper(str) == str {
+	// 	answer++
+	// }
+	//}
+
+	// Solution 3 (my own one)
+	foundWords := 0
 	for i := 0; i < len(input); i++ {
 		charVariable := input[i]
 		if charVariable < 96 {
 			foundWords++
 		}
-		//fmt.Println(charVariable)
 	}
-
 	fmt.Printf("The input string had %d words.", foundWords+1)
 }
