4768aaaea9055231dd6d7d7402216b9c9311cab9
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-24 16:45:20 +0100

    use regexp instead

--- phone/main.go
+++ phone/main.go
@@ -1,14 +1,19 @@
 package main
 
-import "bytes"
+import "regexp"
 
 func normalize(phone string) string {
-	var buf bytes.Buffer
-	for _, ch := range phone {
-		if ch >= '0' && ch <= '9' {
-			buf.WriteRune(ch)
-		}
-	}
-
-	return buf.String()
+	re := regexp.MustCompile("[^0-9]")
+	return re.ReplaceAllString(phone, "")
 }
+
+// func normalize(phone string) string {
+// 	var buf bytes.Buffer
+// 	for _, ch := range phone {
+// 		if ch >= '0' && ch <= '9' {
+// 			buf.WriteRune(ch)
+// 		}
+// 	}
+//
+// 	return buf.String()
+// }
