e423e9686a2439948e4cb0f1da539c5955141925
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-24 21:30:08 +0100

    insert records

--- phone/main.go
+++ phone/main.go
@@ -18,6 +18,8 @@ const (
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s sslmode=disable", host, port, user)
+
+	// ###### Creating/Resetting Database ########
 	db, err := sql.Open("postgres", psqlInfo)
 	must(err)
 
@@ -25,6 +27,7 @@ func main() {
 	must(err)
 
 	db.Close()
+	// ###########################################
 
 	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
 	db, err = sql.Open("postgres", psqlInfo)
@@ -33,12 +36,38 @@ func main() {
 
 	// must(db.Ping())
 	must(createPhoneNumbersTable(db))
+	_, err = insertPhone(db, "1234567890")
+	must(err)
+	_, err = insertPhone(db, "123 456 7891")
+	must(err)
+	_, err = insertPhone(db, "(123) 456 7892")
+	must(err)
+	_, err = insertPhone(db, "(123) 456-7893")
+	must(err)
+	_, err = insertPhone(db, "123-456-7894")
+	must(err)
+	_, err = insertPhone(db, "123-456-7890")
+	must(err)
+	_, err = insertPhone(db, "1234567892")
+	must(err)
+	_, err = insertPhone(db, "(123)456-7892")
+	must(err)
 }
 
 func must(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func insertPhone(db *sql.DB, phone string) (int, error) {
+	statement := `INSERT INTO phone_numbers(value) VALUES($1) RETURNING id`
+	var id int
+	err := db.QueryRow(statement, phone).Scan(&id)
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
 }
 
 func createPhoneNumbersTable(db *sql.DB) error {
