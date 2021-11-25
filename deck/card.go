6d878291c249735bdadd13c8faf6834a664622d0
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-25 23:21:22 +0100

    init commit

---
+++ deck/card.go
@@ -0,0 +1,39 @@
+package deck
+
+type Suit uint8
+
+const (
+	Spade Suit = iota
+	Diamond
+	Club
+	Heart
+	Joker
+)
+
+type Rank uint8
+
+const (
+	_ Rank = iota
+	Ace
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+)
+
+type Card struct {
+	Suit
+	Rank
+}
+
+func (c Card) String() string {
+	return "Ace of Spades"
+}
