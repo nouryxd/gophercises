190fc585a7dbdf34c6a62e8a39d22b85df52c697
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-25 21:06:34 +0100

    refactor

--- phone/db/phone.go
+++ phone/db/phone.go
@@ -1,4 +1,4 @@
-package phonedb
+package db
 
 import (
 	"database/sql"
--- phone/main.go
+++ phone/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"regexp"
 
 	_ "github.com/lib/pq"
-	phonedb "github.com/lyx0/gophercises/phone/phonedb"
+	phonedb "github.com/lyx0/gophercises/phone/db"
 )
 
 const (
@@ -32,58 +31,61 @@ func main() {
 		panic(err)
 	}
 
+	phones, err := db.AllPhones()
+	must(err)
+	for _, p := range phones {
+		fmt.Printf("Working on... %+v\n", p)
+		number := normalize(p.Number)
+		if number != p.Number {
+			fmt.Println("Updating or removing...", number)
+			// existing, err := findPhone(db, number)
+			// must(err)
+			// if existing != nil {
+			// 	// delete this number
+			// 	must(deletePhone(db, p.id))
+			// 	fmt.Printf("Deleted ID %d\n", p.id)
+			// } else {
+			// 	// update this number
+			// 	p.number = number
+			// 	must(updatePhone(db, p))
+			// 	fmt.Printf("Updated ID %d\n", p.id)
+			// }
+		} else {
+			fmt.Println("No changes required")
+		}
+		// fmt.Printf("ID: %d, Number: %s\n", p.id, p.number)
+	}
+
 	// phones, err := allPhones(db)
 	// must(err)
-	// for _, p := range phones {
-	// 	fmt.Printf("Working on... %+v\n", p)
-	// 	number := normalize(p.number)
-	// 	if number != p.number {
-	// 		fmt.Println("Updating or removing...", number)
-	// 		existing, err := findPhone(db, number)
-	// 		must(err)
-	// 		if existing != nil {
-	// 			// delete this number
-	// 			must(deletePhone(db, p.id))
-	// 			fmt.Printf("Deleted ID %d\n", p.id)
-	// 		} else {
-	// 			// update this number
-	// 			p.number = number
-	// 			must(updatePhone(db, p))
-	// 			fmt.Printf("Updated ID %d\n", p.id)
-	// 		}
-	// 	} else {
-	// 		fmt.Println("No changes required")
-	// 	}
-	// 	// fmt.Printf("ID: %d, Number: %s\n", p.id, p.number)
-	// }
 }
 
-func findPhone(db *sql.DB, number string) (*phone, error) {
-	var p phone
-	row := db.QueryRow("SELECT * FROM phone_numbers WHERE value=$1", number)
-	err := row.Scan(&p.id, &p.number)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return nil, nil
-		} else {
-			return nil, err
-		}
-	}
-	return &p, nil
-}
-
-func deletePhone(db *sql.DB, id int) error {
-	statement := `DELETE FROM phone_numbers WHERE id=$1`
-	_, err := db.Exec(statement, id)
-	return err
-
-}
-
-func updatePhone(db *sql.DB, p phone) error {
-	statement := `UPDATE phone_numbers SET value=$2 WHERE id=$1`
-	_, err := db.Exec(statement, p.id, p.number)
-	return err
-}
+// func findPhone(db *sql.DB, number string) (*phone, error) {
+// 	var p phone
+// 	row := db.QueryRow("SELECT * FROM phone_numbers WHERE value=$1", number)
+// 	err := row.Scan(&p.id, &p.number)
+// 	if err != nil {
+// 		if err == sql.ErrNoRows {
+// 			return nil, nil
+// 		} else {
+// 			return nil, err
+// 		}
+// 	}
+// 	return &p, nil
+// }
+//
+// func updatePhone(db *sql.DB, p phone) error {
+// 	statement := `UPDATE phone_numbers SET value=$2 WHERE id=$1`
+// 	_, err := db.Exec(statement, p.id, p.number)
+// 	return err
+// }
+//
+// func deletePhone(db *sql.DB, id int) error {
+// 	statement := `DELETE FROM phone_numbers WHERE id=$1`
+// 	_, err := db.Exec(statement, id)
+// 	return err
+//
+// }
 
 func must(err error) {
 	if err != nil {
