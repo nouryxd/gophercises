fb7273dc84c99e866bd787e12114b45c79d4654f
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-26 00:47:13 +0100

    add more tests

--- deck/card_test.go
+++ deck/card_test.go
@@ -4,7 +4,19 @@ import "fmt"
 
 func ExampleCard() {
 	fmt.Println(Card{Rank: Ace, Suit: Heart})
+	fmt.Println(Card{Rank: Two, Suit: Spade})
+	fmt.Println(Card{Rank: Nine, Suit: Heart})
+	fmt.Println(Card{Rank: Jack, Suit: Club})
+	fmt.Println(Card{Rank: Queen, Suit: Diamond})
+	fmt.Println(Card{Rank: Three, Suit: Spade})
+	fmt.Println(Card{Suit: Joker})
 
 	// Output:
 	// Ace of Hearts
+	// Two of Spades
+	// Nine of Hearts
+	// Jack of Clubs
+	// Queen of Diamonds
+	// Three of Spades
+	// Joker
 }
