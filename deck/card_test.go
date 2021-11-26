a259b43519f142bc108d2ca28460e94d400ec088
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-26 15:55:16 +0100

    add jokers

--- deck/card_test.go
+++ deck/card_test.go
@@ -46,3 +46,16 @@ func TestSort(t *testing.T) {
 		t.Error("Expected Ace of Spades as first card. Received: ", cards[0])
 	}
 }
+
+func TestJokers(t *testing.T) {
+	cards := New(Jokers(3))
+	count := 0
+	for _, c := range cards {
+		if c.Suit == Joker {
+			count++
+		}
+	}
+	if count != 3 {
+		t.Error("Expected 3 Jokers, received:", count)
+	}
+}
