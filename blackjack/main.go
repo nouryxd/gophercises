b71e037f16f887ca1cb3404348d6a5af6cdefa23
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-27 00:00:16 +0100

    add basic game loop

--- blackjack/main.go
+++ blackjack/main.go
@@ -17,16 +17,36 @@ func (h Hand) String() string {
 	return strings.Join(strs, ", ")
 }
 
+func (h Hand) DealerString() string {
+	return h[0].String() + ", **HIDDEN**"
+}
+
 func main() {
 	cards := deck.New(deck.Deck(3), deck.Shuffle)
 	var card deck.Card
 	var player, dealer Hand
 	for i := 0; i < 2; i++ {
 		for _, hand := range []*Hand{&player, &dealer} {
-			card, cards = cards[0], cards[1:]
+			card, cards = draw(cards)
 			*hand = append(*hand, card)
 		}
 	}
-	fmt.Println("Player: ", player)
-	fmt.Println("Dealer: ", dealer)
+	var input string
+	for input != "s" {
+		fmt.Println("Player: ", player)
+		fmt.Println("Dealer: ", dealer.DealerString())
+		fmt.Println("What will you do? (h)it, (s)tand")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			card, cards = draw(cards)
+			player = append(player, card)
+		}
+
+	}
+
+}
+
+func draw(cards []deck.Card) (deck.Card, []deck.Card) {
+	return cards[0], cards[1:]
 }
