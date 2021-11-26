0b9cf1507a72269c0d4ea36be436e860a7e4071f
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-26 23:39:53 +0100

    drawing cards

--- blackjack/main.go
+++ blackjack/main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	deck "github.com/lyx0/gophercises/deck"
 )
+
+type Hand []deck.Card
+
+func (h Hand) String() string {
+	strs := make([]string, len(h))
+	for i := range h {
+		strs[i] = h[i].String()
+	}
+	return strings.Join(strs, ", ")
+}
 
 func main() {
 	cards := deck.New(deck.Deck(3), deck.Shuffle)
@@ -13,4 +24,6 @@ func main() {
 		card, cards = cards[0], cards[1:]
 		fmt.Println(card)
 	}
+	var h Hand = cards[0:3]
+	fmt.Println(h)
 }
