efaa99303e4699dfd1f219dd2a14df84245a888e
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-26 23:46:46 +0100

    draw cards for dealer and player

--- blackjack/main.go
+++ blackjack/main.go
@@ -20,10 +20,13 @@ func (h Hand) String() string {
 func main() {
 	cards := deck.New(deck.Deck(3), deck.Shuffle)
 	var card deck.Card
-	for i := 0; i < 10; i++ {
-		card, cards = cards[0], cards[1:]
-		fmt.Println(card)
+	var player, dealer Hand
+	for i := 0; i < 2; i++ {
+		for _, hand := range []*Hand{&player, &dealer} {
+			card, cards = cards[0], cards[1:]
+			*hand = append(*hand, card)
+		}
 	}
-	var h Hand = cards[0:3]
-	fmt.Println(h)
+	fmt.Println("Player: ", player)
+	fmt.Println("Dealer: ", dealer)
 }
