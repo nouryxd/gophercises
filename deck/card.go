35858cf62a852f9526b037b636982e7897f6c3cf
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-26 02:10:05 +0100

    add sorting

--- deck/card.go
+++ deck/card.go
@@ -2,7 +2,10 @@
 
 package deck
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type Suit uint8
 
@@ -65,6 +68,18 @@ func New(opts ...func([]Card) []Card) []Card {
 		cards = opt(cards)
 	}
 	return cards
+}
+
+func DefaultSort(cards []Card) []Card {
+	sort.Slice(cards, Less(cards))
+	return cards
+}
+
+func Sort(less func(cards []Card) func(i, j int) bool) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		sort.Slice(cards, less(cards))
+		return cards
+	}
 }
 
 func Less(cards []Card) func(i, j int) bool {
