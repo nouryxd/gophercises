87432be2a385299a95ebc5da635905748481074a
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-26 21:50:13 +0100

    draw 10 cards

--- blackjack/go.mod
+++ blackjack/go.mod
@@ -1,3 +1,5 @@
 module github.com/lyx0/gophercises/blackjack
 
 go 1.17
+
+require github.com/lyx0/gophercises/deck v0.0.0-20211126204400-da38d1675dd3
--- blackjack/main.go
+++ blackjack/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	deck "github.com/lyx0/gophercises/deck"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	cards := deck.New(deck.Deck(3), deck.Shuffle)
+	var card deck.Card
+	for i := 0; i < 10; i++ {
+		card, cards = cards[0], cards[1:]
+		fmt.Println(card)
+	}
 }
