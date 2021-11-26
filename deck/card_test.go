8242ee30dd800296940f18fc1da4672d0a843b6c
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-26 01:04:35 +0100

    add test for New card deck

--- deck/card_test.go
+++ deck/card_test.go
@@ -1,6 +1,9 @@
 package deck
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 func ExampleCard() {
 	fmt.Println(Card{Rank: Ace, Suit: Heart})
@@ -19,4 +22,12 @@ func ExampleCard() {
 	// Queen of Diamonds
 	// Three of Spades
 	// Joker
+}
+
+func TestNew(t *testing.T) {
+	cards := New()
+	// 13 ranks * 4 suits
+	if len(cards) != 13*4 {
+		t.Error("Wrong number of cards in a new deck")
+	}
 }
