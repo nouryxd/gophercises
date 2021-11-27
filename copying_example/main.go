5e88f96b4150db328cd7be6837ede7b09fbca04c
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-27 16:12:19 +0100

    add copying example

--- copying_example/main.go
+++ copying_example/main.go
@@ -12,11 +12,17 @@ func main() {
 	d.Name = "jon"
 	d.Numbers = []int{1, 5, 8}
 
-	d2 := d
+	d2 := Demo{
+		Name:    d.Name,
+		Numbers: make([]int, len(d.Numbers)),
+	}
+	copy(d2.Numbers, d.Numbers)
+
 	fmt.Println("original d2=", d2)
 	d2.Name = "bob"
+	d2.Numbers = append(d2.Numbers, 123)
 	d2.Numbers[0], d2.Numbers[1] = d2.Numbers[1], d2.Numbers[0]
-	d2.Numbers = []int{4, 5, 6}
+
 	fmt.Println("d =", d)
 	fmt.Println("d2 =", d2)
 }
