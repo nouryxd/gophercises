baa79e2dab0c08f63b415d0beb0790fcb1af0c43
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-27 15:17:15 +0100

    basic game loop

--- blackjack/main.go
+++ blackjack/main.go
@@ -74,6 +74,13 @@ func main() {
 			player = append(player, card)
 		}
 	}
+	// If dealer score <= 16, we hit
+	// If dealer has a soft 17, then we hit. (Ace as
+	// 11 points and 16 points)
+	for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
+		card, cards = draw(cards)
+		dealer = append(dealer, card)
+	}
 	pScore, dScore := player.Score(), dealer.Score()
 	fmt.Println("=====FINAL HANDS=====")
 	fmt.Println("Player:", player, "\nScore:", pScore)
@@ -89,7 +96,6 @@ func main() {
 		fmt.Println("You lose")
 	case dScore == pScore:
 		fmt.Println("Draw")
-
 	}
 
 }
