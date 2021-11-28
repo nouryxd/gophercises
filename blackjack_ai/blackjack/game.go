d085c19ea0f3a48d9da60a6e0c53f5b8f7ebac2c
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-28 13:27:07 +0100

    refactor

--- blackjack_ai/blackjack/game.go
+++ blackjack_ai/blackjack/game.go
@@ -1,8 +1,6 @@
 package blackjack
 
 import (
-	"fmt"
-
 	"github.com/lyx0/gophercises/deck"
 )
 
@@ -23,17 +21,17 @@ type Game struct {
 
 type Move func(*Game) Game
 
-func Hit(gs *Game) {
+func Hit(g *Game) {
 }
 
-func Stand(gs *Game) {
+func Stand(g *Game) {
 }
 
 func draw(cards []deck.Card) (deck.Card, []deck.Card) {
 	return cards[0], cards[1:]
 }
 
-func Deal(g *Game) {
+func deal(g *Game) {
 	g.player = make([]deck.Card, 0, 5)
 	g.dealer = make([]deck.Card, 0, 5)
 	var card deck.Card
@@ -50,37 +48,67 @@ func (g *Game) Play(ai AI) {
 	g.deck = deck.New(deck.Deck(3), deck.Shuffle)
 
 	for i := 0; i < 10; i++ {
-		gs = Deal(gs)
+		deal(g)
 
-		var input string
-		for gs.State == StatePlayerTurn {
-			fmt.Println("Player: ", gs.Player)
-			fmt.Println("Dealer: ", gs.Dealer.DealerString())
-			fmt.Printf("Your cards are worth %d\n", gs.Player.Score())
-			fmt.Println("What will you do? (h)it, (s)tand")
-			fmt.Scanf("%s\n", &input)
-			fmt.Printf("\n")
-			switch input {
-			case "h":
-				gs = Hit(gs)
-			case "s":
-				gs = Stand(gs)
-			default:
-				fmt.Println("Invalid option:", input)
-			}
+		for g.state == statePlayerTurn {
+			hand := make([]deck.Card, len(g.player))
+			copy(hand, g.player)
+			move := ai.Play(hand, g.dealer[0])
+			move(g)
 		}
 
 		// If dealer score <= 16, we hit
 		// If dealer has a soft 17, then we hit. (Ace as
 		// 11 points and 16 points)
-		for gs.State == StateDealerTurn {
-			if gs.Dealer.Score() <= 16 || (gs.Dealer.Score() == 17 && gs.Dealer.MinScore() != 17) {
-				gs = Hit(gs)
+		for g.state == stateDealerTurn {
+			if g.dealer.Score() <= 16 || (g.dealer.Score() == 17 && g.dealer.MinScore() != 17) {
+				g = Hit(g)
 			} else {
-				gs = Stand(gs)
+				g = Stand(g)
 			}
 		}
 
-		gs = EndHand(gs)
+		g = EndHand(g)
 	}
+}
+
+func Score(hand ...deck.Card) int {
+	minScore := minScore(hand...)
+	if minScore > 11 {
+		return minScore
+	}
+	for _, c := range h {
+		if c.Rank == deck.Ace {
+			// Ace is currently worth 1 point, here we
+			// change it so that it's worth 11
+			return minScore + 10
+		}
+	}
+	return minScore
+}
+
+func Soft(hand ...deck.Card) bool {
+	minScore := minScore(hand...)
+	score := Score(hand...)
+	return minScore != score
+	if minScore != score {
+		return true
+	}
+	return false
+}
+
+func minScore(hand ...deck.Card) int {
+	score := 0
+
+	for _, c := range hand {
+		score += min(int(c.Rank), 10)
+	}
+	return score
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
