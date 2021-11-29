ee93fb5df5567b814f38e5307d6528fe33fc32d2
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-30 00:39:58 +0100

    add impl

--- blackjack_ai/go.mod
+++ blackjack_ai/go.mod
@@ -3,3 +3,11 @@ module github.com/lyx0/gophercises/blackjack_ai
 go 1.17
 
 require github.com/lyx0/gophercises/deck v0.0.0-20211127170404-d98cd122771c
+
+require (
+	github.com/josharian/impl v1.1.0 // indirect
+	golang.org/x/mod v0.5.1 // indirect
+	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
+	golang.org/x/tools v0.1.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
--- blackjack_ai/main.go
+++ blackjack_ai/main.go
@@ -4,7 +4,20 @@ import (
 	"fmt"
 
 	"github.com/lyx0/gophercises/blackjack_ai/blackjack"
+	"github.com/lyx0/gophercises/deck"
 )
+
+func (ai *basicAI) Bet(shuffled bool) int {
+	panic("not implemented") // TODO: Implement
+}
+
+func (ai *basicAI) Play(hand []deck.Card, dealer deck.Card) blackjack.Move {
+	panic("not implemented") // TODO: Implement
+}
+
+func (ai *basicAI) Results(hands [][]deck.Card, dealer []deck.Card) {
+	panic("not implemented") // TODO: Implement
+}
 
 func main() {
 	opts := blackjack.Options{
