607edf319e83f8ff44321cf8aa090737a1d602a0
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-30 18:03:13 +0100

    match

--- renamer/main.go
+++ renamer/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	fileName := "birthday_001.txt"
+	// => Birthday - 1 of 4.txt
+	newName, err := match(fileName)
+	if err != nil {
+		fmt.Println("no match")
+		os.Exit(1)
+	}
+	fmt.Println(newName)
+}
+
+func match(fileName string) (string, error) {
+	// "birthday", "001", "txt"
+
+	return "", nil
 }
