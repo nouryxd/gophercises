a00e9e72a9ea093d82354455760cae0c9972678b
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-30 01:16:21 +0100

    add basic ai

--- blackjack_ai/main.go
+++ blackjack_ai/main.go
@@ -7,25 +7,46 @@ import (
 	"github.com/lyx0/gophercises/deck"
 )
 
+type basicAI struct{}
+
 func (ai *basicAI) Bet(shuffled bool) int {
-	panic("not implemented") // TODO: Implement
+	return 100
 }
 
 func (ai *basicAI) Play(hand []deck.Card, dealer deck.Card) blackjack.Move {
-	panic("not implemented") // TODO: Implement
+	score := blackjack.Score(hand...)
+	if len(hand) == 2 {
+		if hand[0] == hand[1] {
+			cardScore := blackjack.Score(hand[0])
+			if cardScore >= 8 && cardScore != 10 {
+				return blackjack.MoveSplit
+			}
+		}
+		if (score == 10 || score == 11) && !blackjack.Soft(hand...) {
+			return blackjack.MoveDouble
+		}
+	}
+	dScore := blackjack.Score(dealer)
+	if dScore >= 5 && dScore <= 6 {
+		return blackjack.MoveStand
+	}
+	if score < 13 {
+		return blackjack.MoveHit
+	}
+	return blackjack.MoveStand
 }
 
 func (ai *basicAI) Results(hands [][]deck.Card, dealer []deck.Card) {
-	panic("not implemented") // TODO: Implement
+	// noop
 }
 
 func main() {
 	opts := blackjack.Options{
-		Decks:           3,
-		Hands:           1,
+		Decks:           4,
+		Hands:           5000000,
 		BlackjackPayout: 1.5,
 	}
 	game := blackjack.New(opts)
-	winnings := game.Play(blackjack.HumanAI())
+	winnings := game.Play(&basicAI{})
 	fmt.Println(winnings)
 }
