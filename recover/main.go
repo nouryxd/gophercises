d1a9bebcb0e37e2b4cb45738ac3263891c73f381
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-12-20 18:38:12 +0100

    ResponseWriter wrapper

--- recover/main.go
+++ recover/main.go
@@ -26,12 +26,49 @@ func recoverMw(app http.Handler, dev bool) http.HandlerFunc {
 					http.Error(rw, "Something went wrong :(", http.StatusInternalServerError)
 					return
 				}
+				rw.WriteHeader(http.StatusInternalServerError)
 				fmt.Fprintf(rw, "<h1>Panic: %s</h1><pre>%s</pre>", err, string(stack))
 			}
 		}()
 
-		app.ServeHTTP(rw, r)
+		nrw := &responseWriter{ResponseWriter: rw}
+		app.ServeHTTP(nrw, r)
+		nrw.flush()
 	}
+}
+
+// type ResponseWriter interface {
+// 	Header()
+// 	Write([]byte) (int, error)
+// 	WriteHeader(statusCode int)
+// }
+
+type responseWriter struct {
+	http.ResponseWriter
+	writes [][]byte
+	status int
+}
+
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	rw.writes = append(rw.writes, b)
+	return len(b), nil
+}
+
+func (rw *responseWriter) WriteHeader(statusCode int) {
+	rw.status = statusCode
+}
+
+func (rw *responseWriter) flush() error {
+	if rw.status != 0 {
+		rw.ResponseWriter.WriteHeader(rw.status)
+	}
+	for _, write := range rw.writes {
+		_, err := rw.ResponseWriter.Write(write)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func panicDemo(w http.ResponseWriter, r *http.Request) {
